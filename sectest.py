c5a6c4aa40ac8fcc8dcef840f68f360f1673555e
ronegray <ronegray@gmail.com>
2026-01-21 14:49:30 +0900

    for error test 4

---
+++ sectest.py
@@ -0,0 +1,16 @@
+AWS_SECRET_KEY = "AKIA1234567890EXAMPLE"
+
+TOKEN = "ghp_000000000000000000000000000000000000"
+AWS_KEY = "AKIAQAAAAAAABBBBBCCC"
+
+match AWS_SECRET_KEY:
+    case "0":
+        pass
+    case "1":
+        pass
+    case "2":
+        print(0)
+
+def secs(a:int):
+    return a
+
