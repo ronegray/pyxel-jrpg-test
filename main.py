4a0cbeac92647c77bfb7a3d5fe25aa715d0ccfed
ronegray <ronegray@gmail.com>
2026-01-21 14:16:44 +0900

    for error test

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     print("Hello from pyxel-jrpg-test!")
 
+AWS_SECRET_KEY = "AKIA1234567890EXAMPLE"
+
+match AWS_SECRET_KEY:
+    case "0":
+        pass
+    case "1":
+        pass
 
 if __name__ == "__main__":
     main()
